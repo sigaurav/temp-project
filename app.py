73c80da1d717385ed70ff036c67cb5cc27f25d60
sigaurav <grv.skrwr0562@gmail.com>
2022-11-07 09:44:42 +0530

    Updated For Formula Values

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
 import tkinter as tk
 from tkinter import ttk, messagebox
 from metadata import color_pallet
 from tkinter import filedialog as fd
 from main import SegregateFiles
+from main import MergeFiles
 
 filepath = ''
 
@@ -52,7 +54,7 @@ class OpenFileUI(tk.Tk):
         self.title_frame.grid(row=0, column=0)
 
         title = ttk.Label(self.title_frame, text='Split Files', style='Title.TLabel')
-        title.grid(row=0, column=0, sticky='WE', padx=(160, 160), pady=(5, 5))
+        title.grid(row=0, column=0, sticky='WE', padx=(160, 90), pady=(5, 5))
 
         self.body_frame = ttk.Frame(self, style='Analysis.TFrame')
         self.body_frame.grid(row=1, column=0, sticky='WE', padx=(10, 10), pady=(10, 10))
@@ -60,14 +62,40 @@ class OpenFileUI(tk.Tk):
         load_file_button = ttk.Button(self.body_frame, text='Browse File', cursor='hand2', command=self.select_file)
         load_file_button.grid(row=2, column=0, sticky='WE', padx=(10, 10), pady=(20, 20))
 
-        generate_files_button = ttk.Button(self.body_frame, text='Generate Files', cursor='hand2',
+        generate_files_button = ttk.Button(self.body_frame, text='Split Files', cursor='hand2',
                                            command=self.generate_files)
         generate_files_button.grid(row=2, column=1, sticky='WE', padx=(10, 10), pady=(20, 20))
+
+        merge_files_button = ttk.Button(self.body_frame, text='Merge Files', cursor='hand2',
+                                           command=self.merge_files)
+        merge_files_button.grid(row=2, column=2, sticky='WE', padx=(10, 10), pady=(20, 20))
+
+
+    def merge_files(self):
+        label_loading = ttk.Label(self.body_frame, text='Status: Select directory...', style='Body.TLabel')
+        label_loading.grid(row=3, column=0, sticky='WE', padx=(10, 10), pady=(10, 10))
+
+        file_dir = fd.askdirectory()
+        if file_dir == '' or file_dir is None:
+            messagebox.showerror('Missing Path', 'No Directory Selected')
+            exit(0)
+        try:
+            MergeFiles.merge_files(file_dir)
+        except Exception as e:
+            label_loading = ttk.Label(self.body_frame, text='Status: Failed', style='Body.TLabel')
+            label_loading.grid(row=3, column=0, sticky='WE', padx=(10, 10), pady=(10, 10))
+            tk.messagebox.showerror('Failed', e)
+
+        label_loading = ttk.Label(self.body_frame, text='Status: Successful', style='Body.TLabel')
+        label_loading.grid(row=3, column=0, sticky='WE', padx=(10, 10), pady=(10, 10))
+        messagebox.showinfo('Status:', 'Process Completed')
+        exit(0)
+
 
     def select_file(self):
         global filepath
         label_loading = ttk.Label(self.body_frame, text='Status: Loading . . .', style='Body.TLabel')
-        label_loading.grid(row=2, column=2, sticky='WE', padx=(10, 10), pady=(10, 10))
+        label_loading.grid(row=3, column=0, sticky='WE', padx=(10, 10), pady=(10, 10))
 
         filetypes = (
             ('Excel', '.*xlsx'),
@@ -78,27 +106,37 @@ class OpenFileUI(tk.Tk):
             title='Select File', filetypes=filetypes
         )
 
-        label_loading = ttk.Label(self.body_frame, text='Status: File Loaded', style='Body.TLabel')
-        label_loading.grid(row=2, column=2, sticky='WE', padx=(10, 10), pady=(10, 10))
+        if filepath == '' or filepath is None:
+            label_loading = ttk.Label(self.body_frame, text='Status: No File Selected', style='Body.TLabel')
+            label_loading.grid(row=3, column=0, sticky='WE', padx=(10, 10), pady=(10, 10))
+        else:
+            label_loading = ttk.Label(self.body_frame, text='Status: File Loaded', style='Body.TLabel')
+            label_loading.grid(row=3, column=0, sticky='WE', padx=(10, 10), pady=(10, 10))
 
     def generate_files(self):
-        label_loading = ttk.Label(self.body_frame, text='Status: Creating Files . . .', style='Body.TLabel')
-        label_loading.grid(row=2, column=2, sticky='WE', padx=(10, 10), pady=(10, 10))
+
+        if filepath == '' or filepath is None:
+            messagebox.showerror('Missing File', 'No Input found')
+            exit(0)
+        else:
+            label_loading = ttk.Label(self.body_frame, text='Status: Creating Files . . .', style='Body.TLabel')
+            label_loading.grid(row=3, column=0, sticky='WE', padx=(10, 10), pady=(10, 10))
 
         output_path = fd.askdirectory()
-        SegregateFiles.process_masterfile(filepath, output_path)
+        if output_path == '' or output_path is None:
+            messagebox.showerror('Missing Path', 'No Output Path Given')
+            exit(0)
+        try:
+            SegregateFiles.process_masterfile(filepath, output_path)
+        except Exception as e:
+            label_loading = ttk.Label(self.body_frame, text='Status: Failed', style='Body.TLabel')
+            label_loading.grid(row=3, column=0, sticky='WE', padx=(10, 10), pady=(10, 10))
+            tk.messagebox.showerror('Failed', e)
 
         label_loading = ttk.Label(self.body_frame, text='Status: Successful', style='Body.TLabel')
-        label_loading.grid(row=2, column=2, sticky='WE', padx=(10, 10), pady=(10, 10))
+        label_loading.grid(row=3, column=0, sticky='WE', padx=(10, 10), pady=(10, 10))
+        messagebox.showinfo('Status:', 'Process Completed')
         exit(0)
-
-
-# def main():
-#     file = OpenFileUI()
-#
-#
-# if __name__ == '__main__':
-#     main()
 
 file = OpenFileUI()
 file.mainloop()
--- main.py
+++ main.py
@@ -1,25 +1,109 @@
+from openpyxl import load_workbook
 import pandas as pd
+from openpyxl.formula.translate import Translator
+from openpyxl.utils.cell import get_column_letter
 import os
 import support as sp
 
+
 class SegregateFiles:
     managers_lst = []
-
+    blank_cols = 0
+    formula_column = {}
     def process_masterfile(filepath: str, output_path: str):
         """:param filepath: Path of input file
         :param output_path: output directory"""
 
         file_name = os.path.basename(filepath)
         print(file_name)
-        master_df = pd.read_excel(filepath, index_col=False)
+        wb = load_workbook(filepath)
+        sheets = wb.sheetnames
+        sheet_name = wb[sheets[0]]
+        master_df = pd.DataFrame(sheet_name.values)
+        master_df.columns = master_df.iloc[0]
+        master_df = master_df[1:]
+        master_df.reset_index()
+
 
         managers_lst = master_df.Manager.unique().tolist()
-
+        SegregateFiles.formula_column = SegregateFiles.get_formula_columns(master_df)
         for manager in managers_lst:
             temp_df = master_df[master_df['Manager'] == manager]
+            temp_df.reset_index(inplace=True, drop=True)
+            SegregateFiles.update_formula(temp_df, SegregateFiles.formula_column)
             op_file_name = manager + "_" + file_name
             sp.export_file(temp_df, output_path + "/" + op_file_name)
 
+
+    def update_formula(df: pd.DataFrame, formula_col: str):
+        '''
+        Function to update formulas for each Manager
+        :param df:  DataFrame for one specific manager.
+        '''
+        for _col in formula_col:
+            col_alpha = formula_col[_col][0]
+            formula = formula_col[_col][1]
+            index = 2
+            for ind, row in df.iterrows():
+                df.at[ind, _col] = Translator(formula, origin=col_alpha + '2').translate_formula(col_alpha + str(index))
+                index = index + 1
+
+
+    def get_formula_columns(df: pd.DataFrame):
+        '''
+        Function to find formulae based columns and rename blank columns
+        @param: df master df
+        '''
+
+        formula_cols = {}
+        for i in range(len(df.columns.tolist())):
+            col = df.columns[i]
+            if pd.isna(col) or str(col).strip() == '':
+                df.rename(columns={col: 'BlankCol' + str(SegregateFiles.blank_cols)}, inplace=True)
+                col = 'BlankCol' + str(SegregateFiles.blank_cols)
+                SegregateFiles.blank_cols = SegregateFiles.blank_cols + 1
+
+            col_value = str(df[df.columns[i]].values[0])
+            if col_value.startswith('='):
+                if col not in formula_cols:
+                    formula_cols[col] = [get_column_letter(i+1), col_value]
+                else:
+                    df.rename(columns={col: col + "1"}, inplace=True)
+                    formula_cols[col + "1"] = [get_column_letter(i+1), col_value]
+
+        return formula_cols
+
+class MergeFiles:
+
+    df_columns = []
+    formula_column = {}
+    def merge_files(file_dir: str):
+
+        master_df = pd.DataFrame()
+        file_list = os.listdir(file_dir)
+        file_list = [file_dir + '/' + list for list in file_list]
+        # Read all excel files
+        file_num = 1
+        for file in file_list:
+            wb = load_workbook(file)
+            sheet_name = wb[wb.sheetnames[0]]
+            temp_df = pd.DataFrame(sheet_name.values)
+            if file_num == 1:
+                MergeFiles.df_columns = temp_df.iloc[0]
+
+
+            temp_df = temp_df[1:]
+            temp_df.columns = MergeFiles.df_columns
+            master_df = pd.concat([master_df, temp_df], axis=0)
+
+
+        MergeFiles.formula_column = SegregateFiles.get_formula_columns(master_df)
+        master_df.reset_index(inplace=True, drop=True)
+        SegregateFiles.update_formula(master_df, MergeFiles.formula_column)
+
+        sp.export_file(master_df, file_dir + "/" + "Consolidated.xlsx")
+        file_num = file_num + 1
+        print(f'Merged {file_num} files')
 
 
 
